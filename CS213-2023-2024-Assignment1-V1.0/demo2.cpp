662c9732a0aec936c268407d0ee9214620a4631a
Mazen-Abdelfattah <147278273+Mazen-Abdelfattah@users.noreply.github.com>
2023-10-11 21:12:47 +0300

    Update demo2.cpp

--- CS213-2023-2024-Assignment1-V1.0/demo2.cpp
+++ CS213-2023-2024-Assignment1-V1.0/demo2.cpp
@@ -1,8 +1,8 @@
 //FCAI - OOP programing - 2023 - Assignment 1
 // Program: demo2.cpp
-// Author1 and ID : Mazen Abdelfattah /20230607
-// Author2 and ID : Malak Mohamed Khattab/20210403
-// Author3 and ID : Gehad Shaban  Ragab/20220535
+// Author1 and ID : Mazen Abdelfattah - 20230607
+// Author2 and ID : Malak Khattab - 20210403
+// Author3 and ID : Gehad Shaban - 20220535
 // Purpose: Demonstrate use of bmplip for handling
 //          bmp colored and grayscale images
 //          Program load a gray image and store in another file
@@ -20,6 +20,44 @@ unsigned char image2[SIZE][SIZE];
 void loadImage ();
 void saveImage ();
 void doSomethingForImage ();
+void darken_lighten() {
+    int check;
+    cout << "do u want darken-->1 or lighten-->2:";
+    cin >> check;
+    switch (check)
+    {
+        case 1: {
+            for (int i = 0; i < SIZE; i++) {
+                for (int j = 0; j < SIZE; j++) {
+                    if (image[i][j] <= 60) {
+                        image2[i][j] = image[i][j];
+                    }
+                    else {
+                        image2[i][j] = image[i][j] - 60;
+                    }
+                }
+            }
+            break;
+        }
+        case 2: {
+            for (int i = 0; i < SIZE; i++) {
+                for (int j = 0; j < SIZE; j++) {
+                    if (image[i][j] >= 195) {
+                        image2[i][j] = image[i][j];
+                    }
+                    else {
+                        image2[i][j] = image[i][j] + 60;
+                    }
+                }
+            }
+            break;
+        }
+
+        default:
+            darken_lighten();
+            break;
+    }
+}
 
 int main()
 {
@@ -59,13 +97,24 @@ void saveImage () {
 void doSomethingForImage()
 {
     char choice;
-    cout<<"Please select a filter to apply :"<<endl;
+    cout<<"Please select a filter to apply or 0 to exit"<<endl;
     cout<<"1-Black & White Filter"<<endl;
     cout<<"2-Invert Filter"<<endl;
     cout<<"3-Merge Filter"<<endl;
     cout<<"4-Flip Image"<<endl;
     cout<<"5-Rotate Image"<<endl;
     cout<<"6-Darken and Lighten Image"<<endl;
+    cout<<"7-Detect Image Edges"<<endl;
+    cout<<"8-Enlarge Image"<<endl;
+    cout<<"9-Shrink Image"<<endl;
+    cout<<"a-Mirror 1/2 Image"<<endl;
+    cout<<"b-Shuffle Image"<<endl;
+    cout<<"c-Blur Image"<<endl;
+    cout<<"d-Crop Image"<<endl;
+    cout<<"e-Skew Image Right"<<endl;
+    cout<<"f-Skew Image Up"<<endl;
+    cout<<"s- Save the image to a file"<<endl; //I have no idea how to code this
+    cout<<"0-Exit"<<endl; //I have no idea how to code this
     cin>>choice;
     if(choice=='1')
     {
@@ -210,48 +259,202 @@ void doSomethingForImage()
         }
     }
     else if(choice=='6'){
-          int check;
-        cout << "do u want darken-->1 or lighten-->2:";
+        int check;
+        cout << "do u want darken-->1 or lighten-->2: ";
         cin >> check;
-        switch (check) {
-            case 1: {
-                for (int i = 0; i < SIZE; i++) {
-                    for (int j = 0; j < SIZE; j++) {
-                        image2[i][j] = image[i][j];
-                    }
+        if(check==1)
+        {
+            for (int i = 0; i < SIZE; i++)
+            {
+                for (int j = 0; j < SIZE; j++)
+                {
+                    image2[i][j] = image[i][j];
                 }
-                for (int i = 0; i < SIZE; i++) {
-                    for (int j = 0; j < SIZE; j++) {
-                        if (image2[i][j] <= 60) {
-                            image[i][j] = image2[i][j];
-                        } else {
-                            image[i][j] = image2[i][j] - 60;
-                        }
-                    }
-                }
-                break;
             }
-
-
-            case 2: {
-                for (int i = 0; i < SIZE; i++) {
-                    for (int j = 0; j < SIZE; j++) {
-                        image2[i][j] = image[i][j];
+            for (int i = 0; i < SIZE; i++) {
+                for (int j = 0; j < SIZE; j++) {
+                    if (image2[i][j] <= 60) {
+                        image[i][j] = image2[i][j];
+                    }
+                    else {
+                        image[i][j] = image2[i][j] - 60;
                     }
                 }
-                for (int i = 0; i < SIZE; i++) {
-                    for (int j = 0; j < SIZE; j++) {
-                        if (image2[i][j] >= 195) {
-                            image[i][j] = image2[i][j];
-                        }
-                        else {
-                            image[i][j] = image2[i][j] + 60;
-                        }
-                    }
-                }
-                break;
             }
-
+        }
+        else if(check==2)
+        {
+            for (int i = 0; i < SIZE; i++)
+            {
+                for (int j = 0; j < SIZE; j++)
+                {
+                    image2[i][j] = image[i][j];
+                }
+            }
+            for (int i = 0; i < SIZE; i++) {
+                for (int j = 0; j < SIZE; j++) {
+                    if (image[i][j] >= 195) {
+                        image[i][j] = image[i][j];
+                    }
+                    else {
+                        image[i][j] = image2[i][j] + 60;
+                    }
+                }
+            }
         }
     }
+    else if(choice=='7')
+    {
+        for (int i = 0; i < SIZE; i++)
+        {
+            for (int j = 0; j < SIZE; j++)
+            {
+                image2[i][j] = image[i][j] ; //Egualize image2 to image
+            }
+        }
+        int avg = 0;
+        for (int i = 0; i < SIZE ; i++)
+        {
+            for (int j = 0; j < SIZE ; j++)
+            {
+                avg += image[i][j];
+            }
+        }
+        avg /= (256*256); //The average color per pixel
+        for (int i = 0; i < SIZE; i++)
+        {
+            //The average will help us to detect whether the pixel is edge or not
+            for (int j = 0; j < SIZE; j++)
+            {
+                //Here we are making sure that the pixel is edge and different from the rest neighbor pixels
+                if( (image2[i][j] > avg && image2[i][j+1] < avg ) || (image2[i][j] < avg && image2[i][j+1] > avg )
+                || (image2[i][j] > avg && image2[i-1][j] < avg) || (image2[i][j] < avg && image2[i-1][j] > avg)
+                || (image2[i][j] > avg && image2[i+1][j+1] < avg) || (image2[i][j] < avg && image2[i+1][j+1] > avg) )
+                {
+                    image[i][j] = 0; //Converting this edge pixel to Black
+                }
+                else
+                {
+                    image[i][j] = 255; //Converting the non-edge pixels to White
+                }
+            }
+        }
+    }
+    else if(choice=='8'){}
+    else if(choice=='9'){}
+    else if(choice=='a')
+    {
+        char letter;
+        cout<<"Mirror (l)eft , (r)ight , (u)pper or (d)own side ?"<<endl;
+        cin>>letter;
+        if(letter=='l')
+        {
+            for (int i = 0; i < SIZE ; i++)
+            {
+                for (int j = 0; j < SIZE ; j++)
+                {
+                    image2[i][j] = image[i][j]; //Equalize image2 to image
+                }
+            }
+            //We want to mirror the left side ,So we will make the right side the same to the left
+            for (int i = 0; i < SIZE ; i++ )
+            {
+                for (int j = 0, z = SIZE - 1; j < z ; j++, z--)
+                {
+                    image[i][z]=image2[i][j];
+                }
+            }
+        }
+        else if(letter=='r')
+        {
+            for (int i = 0; i < SIZE ; i++)
+            {
+                for (int j = 0; j < SIZE ; j++)
+                {
+                    image2[i][j] = image[i][j]; //Equalize image2 to image
+                }
+            }
+            //We want to mirror the right side ,So we will make the left side the same to the right
+            for (int i = 0; i < SIZE ; i++ )
+            {
+                for (int j = SIZE - 1, z = 0 ; j > z ; j--, z++)
+                {
+                    image[i][z]=image2[i][j];
+                }
+            }
+        }
+        else if(letter=='u')
+        {
+            for (int i = 0; i < SIZE ; i++)
+            {
+                for (int j = 0; j < SIZE ; j++)
+                {
+                    image2[i][j] = image[i][j]; //Equalize image2 to image
+                }
+            }
+            //We want to mirror the upper side ,So we will make the lower side the same to the upper
+            for (int i = 0 , z = SIZE - 1 ; i < z ; i++ ,z-- )
+            {
+                for (int j = 0 ; j < SIZE ; j++ )
+                {
+                    image[z][j]=image2[i][j];
+                }
+            }
+        }
+        else if(letter=='d')
+        {
+            for (int i = 0; i < SIZE ; i++)
+            {
+                for (int j = 0; j < SIZE ; j++)
+                {
+                    image2[i][j] = image[i][j]; //Equalize image2 to image
+                }
+            }
+            //We want to mirror the lower side ,So we make the upper side the same to the lower
+            for (int i = SIZE - 1 , z = 0 ; i >z  ; i-- ,z++)
+            {
+                for (int j = 0 ; j < SIZE ; j++ )
+                {
+                    image[z][j]=image2[i][j];
+                }
+            }
+        }
+    }
+    else if(choice=='b'){}
+    else if(choice=='c'){}
+    else if(choice=='d')
+    {
+        int x,y,l,w;
+        cout<<"Please enter x : ";
+        cin>>x;
+        cout<<"Please enter y : ";
+        cin>>y;
+        cout<<"Please enter Length : ";
+        cin>>l;
+        cout<<"Please enter Width : ";
+        cin>>w;
+        for(int i=0 ; i < SIZE ;i++ )
+        {
+            for(int j=0; j < SIZE ;j++ )
+            {
+                if(i<y || i>(y+l) ) //This is the vertical region that does not contain the photo
+                {
+                    image[i][j]=255;
+                }
+                if(j<x || j>(x+w) )//This is the horizontal region that does not contain the photo
+                {
+                    image[i][j]=255;
+                }
+            }
+        }
+
+    }
+    else if(choice=='e'){}
+    else if(choice=='f'){}
+    else
+    {
+        return ;
+    }
+
+
 }
